0921d672e59e7f913e3c791f6a2e0752545c87a2
Stefan Adrian Cozaciuc <stcozaci@c2r6s4.42madrid.com>
2025-10-27 11:59:28 +0100

    ft_printf done

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: stcozaci <stcozaci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 17:39:34 by stcozaci          #+#    #+#             */
-/*   Updated: 2025/10/24 16:46:26 by stcozaci         ###   ########.fr       */
+/*   Updated: 2025/10/27 11:52:43 by stcozaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_putnbr(int n);
 int	ft_putunsign(unsigned int nb);
-int	ft_puthex(unsigned int nb, char *strhex);
+int	ft_puthex(unsigned long long nb, char *strhex);
 #endif
--- ft_puthex.c
+++ ft_puthex.c
@@ -6,13 +6,24 @@
 /*   By: stcozaci <stcozaci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 12:12:50 by stcozaci          #+#    #+#             */
-/*   Updated: 2025/10/24 16:12:57 by stcozaci         ###   ########.fr       */
+/*   Updated: 2025/10/27 11:58:06 by stcozaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_puthex(unsigned int nb, char *strhex)
+int	ft_putp(void *addr)
+{
+	int					len;
+	unsigned long long	nb;
+
+	len = 0;
+	nb = (unsigned long long) addr;
+	len += ft_puthex(nb, "0123456789abcdef");
+	return (len);
+}
+
+int	ft_puthex(unsigned long long nb, char *strhex)
 {
 	int	len;
 
